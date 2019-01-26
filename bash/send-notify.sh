5a77a49feb07741cc0fa8cb034a350316d062ae7
adisbr <adis_brkic@hotmail.com>
2019-01-26 01:12:45 +0100

    25012019: slack added v2

--- bash/send-notify.sh
+++ bash/send-notify.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-#send information about the build
-curl -X POST -H 'Content-type: application/json' \
-    --data "{'text': 'There are failed tests.'}" https://hooks.slack.com/services/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
-
-# send link to the report into slack chat room
-curl -X POST -H 'Content-type: application/json' --data "{
-    'attachments': [
-        {
-            'fallback': 'Failed test data',
-            'text': 'There are failed tests.',
-            'color': '#764FA5'
-        }
-}"  https://hooks.slack.com/services/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
+curl -X POST --data-urlencode "payload={\"channel\": \"#laravel57\", \"username\": \"webhookbot\", \"text\": \"This is posted to #laravel57 and comes from a bot named webhookbot.\", \"icon_emoji\": \":ghost:\"}"https://hooks.slack.com/services/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
 
 
 
